191cebb4ac803217cd3f7a903f9eb2e7b78c51d7
Lukas Messerschmidt <messerschmidt.lukas@gmx.de>
2025-02-01 17:29:35 +0100

    changed theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,11 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+
+html_logo = ""
+
+html_title = f"Modifier Presets {version} Documentation"
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
